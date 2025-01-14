9e78bf65a7e2da85fac8581b03eafe756531742a
annakhizhniak <kukurika.anna@gmail.com>
2025-01-14 16:13:28 +0200

    added signup function, validate function for signin/signup

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "../styles/custom.scss";
 import 'bootstrap';
 import '../styles/style.scss';
-import './modules/signIn.js'
+import './modules/signIn.js';
+import './modules/signUp.js';
 
-console.log("Hello World")
+
